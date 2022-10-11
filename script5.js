efe29dc1f1951ca3f161958ce51f6832db22dafd
Exprandom <113297751+Exprandom@users.noreply.github.com>
2022-10-11 23:20:34 +0300

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,20 @@
+function cost(){
+    
+    var result;
+    var num1=document.getElementById('price').value;
+    var num2 =document.getElementById('quantity').value;
+    let res=document.getElementById('result');
+    if( (isNaN(num1) || num1<0) && ( isNaN(num2)|| num2<0 || (Number.isInteger(num2))))
+    { res.innerHTML = "Введены некорректные данные";}
+    else if( isNaN(num2) || num2<0 || (Number.isInteger(num2)))  res.innerHTML = "Введено неверное количество товара";
+    else if(isNaN(num1) || num1<0)  res.innerHTML = "Введена неверная цена ";
+    else{ var result=num1*num2;
+        res.innerHTML ="Стоимость заказа: "+ result;
+    }
+    return false;
+}
+window.addEventListener('DOMContentLoaded', function (event) {
+    console.log("DOM fully loaded and parsed");
+    let b=document.getElementById("my-button");
+    b.addEventListener("click", cost);
+});
